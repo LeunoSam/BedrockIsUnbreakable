93c5826626a561081458df2a26c21b7a794dac26
LeunoSam <aakeil@web.de>
2023-02-19 18:37:19 +0100

    Edited ObsidianKnifeItem

--- forge-1.19.2-43.2.0-mdk/src/main/java/de/sajomon/bedrock_is_unbreakable/item/custom/ObsidianKnifeItem.java
+++ forge-1.19.2-43.2.0-mdk/src/main/java/de/sajomon/bedrock_is_unbreakable/item/custom/ObsidianKnifeItem.java
@@ -1,23 +1,29 @@
 package de.sajomon.bedrock_is_unbreakable.item.custom;
 
+import de.sajomon.bedrock_is_unbreakable.item.ModItems;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
 public class ObsidianKnifeItem extends Item {
+	
 
 	public ObsidianKnifeItem(Properties props) {
 		super(props);
-		
 	}
 	
 	@Override
 	public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
 		if(!level.isClientSide && hand == InteractionHand.MAIN_HAND) {
-			
+			Inventory i = player.getInventory();
+			if(i.contains(new ItemStack(ModItems.ONION.get()))) {
+				i.add(new ItemStack(ModItems.SLICED_ONION.get()));
+				player.getCooldowns().addCooldown(this, 10);
+			}
 		}
 		return super.use(level, player, hand);
 	}
