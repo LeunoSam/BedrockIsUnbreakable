5119edab3eed22819dacf724e127c766d2e424ea
snydto <sibekei@web.de>
2023-02-19 17:45:36 +0100

    OnionBlock version 0.1

---
+++ forge-1.19.2-43.2.0-mdk/src/main/java/de/sajomon/bedrock_is_unbreakable/block/custom/OnionBlock.java
@@ -0,0 +1,50 @@
+package de.sajomon.bedrock_is_unbreakable.block.custom;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public class OnionBlock extends CropBlock {
+
+	public static final IntegerProperty AGE = IntegerProperty.create("age", 0, 10);
+	private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[]{Block.box(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 3.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 4.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 5.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 6.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 7.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 9.0D, 16.0D)};
+
+
+	public OnionBlock(Properties props) {
+		super(props);
+	}
+
+	@Override
+	protected ItemLike getBaseSeedId() {
+		return Items.POTATO; // TODO implement method
+	}
+
+	@Override
+	public IntegerProperty getAgeProperty() {
+		return AGE;
+	}
+
+	@Override
+	public int getMaxAge() {
+		return 10;
+	}
+
+	@Override 
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+		builder.add(AGE);
+	}
+
+	@Override
+	public VoxelShape getShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context) {
+		return SHAPE_BY_AGE[state.getValue(this.getAgeProperty())];
+	}
+
+}
