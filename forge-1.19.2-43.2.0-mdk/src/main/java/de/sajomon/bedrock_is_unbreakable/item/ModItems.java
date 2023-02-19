ad7a8e917acfd93cdd7067588f94c6225f75cbc4
snydto <sibekei@web.de>
2023-02-19 18:14:33 +0100

    added item Onion

--- forge-1.19.2-43.2.0-mdk/src/main/java/de/sajomon/bedrock_is_unbreakable/item/ModItems.java
+++ forge-1.19.2-43.2.0-mdk/src/main/java/de/sajomon/bedrock_is_unbreakable/item/ModItems.java
@@ -1,6 +1,7 @@
 package de.sajomon.bedrock_is_unbreakable.item;
 
 import de.sajomon.bedrock_is_unbreakable.BedrockIsUnbreakable;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -20,6 +21,11 @@ public class ModItems {
 	
 	public static final RegistryObject<Item> OBSIDIAN_STICK = ITEMS.register("obsidian_stick",
 			() -> new Item(new Item.Properties().tab(ModCreativeModeTab.BEDROCK_IS_UNBREAKABLE_ITEM_TAB)));
+	
+	public static final RegistryObject<Item> ONION = ITEMS.register("onion",
+			() -> new Item(new Item.Properties().tab(ModCreativeModeTab.BEDROCK_IS_UNBREAKABLE_ITEM_TAB)
+					.food(new FoodProperties.Builder().nutrition(2).saturationMod(2f).build()))
+			);
 	
 	public static final RegistryObject<Item> SLICED_ONION = ITEMS.register("sliced_onion",
 			() -> new Item(new Item.Properties().tab(ModCreativeModeTab.BEDROCK_IS_UNBREAKABLE_ITEM_TAB)));
