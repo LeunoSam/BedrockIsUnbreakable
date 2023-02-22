a9886e9a7e70629d6beecf8db8d56303e4d7f975
LeunoSam <aakeil@web.de>
2023-02-22 19:44:02 +0100

    added new tags

---
+++ forge-1.19.2-43.2.0-mdk/src/main/java/de/sajomon/bedrock_is_unbreakable/util/ModTags.java
@@ -0,0 +1,29 @@
+package de.sajomon.bedrock_is_unbreakable.util;
+
+import de.sajomon.bedrock_is_unbreakable.BedrockIsUnbreakable;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
+
+public class ModTags {
+	
+	public static class Blocks {
+		
+		public static final TagKey<Block> NEEDS_OBSIDIAN_TOOL =
+				tag("needs_obsidian_tool");
+		
+		public static final TagKey<Block> NEEDS_OBSIDIAN_STEEL_TOOL =
+				tag("needs_obsidian_steel_tool");
+		
+		
+		private static TagKey<Block> tag(String name) {
+			return BlockTags.create(new ResourceLocation(BedrockIsUnbreakable.MOD_ID, name));
+		}
+		
+		private static TagKey<Block> forgeTag(String name) {
+			return BlockTags.create(new ResourceLocation("forge", name));
+		}
+	}
+	
+}
