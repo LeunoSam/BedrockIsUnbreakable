29bffa7a644ee8affea391d5c58455c75d3c3745
snydto <sibekei@web.de>
2023-02-22 12:52:34 +0100

    added spawn placement register event for BlueSlime

--- forge-1.19.2-43.2.0-mdk/src/main/java/de/sajomon/bedrock_is_unbreakable/BedrockIsUnbreakable.java
+++ forge-1.19.2-43.2.0-mdk/src/main/java/de/sajomon/bedrock_is_unbreakable/BedrockIsUnbreakable.java
@@ -1,40 +1,21 @@
 package de.sajomon.bedrock_is_unbreakable;
 
-import com.mojang.logging.LogUtils;
-
 import de.sajomon.bedrock_is_unbreakable.block.ModBlocks;
 import de.sajomon.bedrock_is_unbreakable.entity.ModEntityTypes;
-import de.sajomon.bedrock_is_unbreakable.entity.client.BlueSlimeRenderer;
 import de.sajomon.bedrock_is_unbreakable.item.ModItems;
-import net.minecraft.client.renderer.ItemBlockRenderTypes;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.entity.EntityRenderers;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.SpawnPlacements;
-import net.minecraft.world.entity.monster.Monster;
-import net.minecraft.world.level.levelgen.Heightmap;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
-import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent.MergedSpawnPredicate;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-import java.util.HashMap;
-import java.util.Map;
 
-import org.slf4j.Logger;
+//import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(BedrockIsUnbreakable.MOD_ID)
-public class BedrockIsUnbreakable
-{
+public class BedrockIsUnbreakable {
 	public static final String MOD_ID = "bedrock_is_unbreakable";
-	private static final Logger LOGGER = LogUtils.getLogger();
+	//private static final Logger LOGGER = LogUtils.getLogger();
 
 	public BedrockIsUnbreakable() {
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -43,30 +24,7 @@ public class BedrockIsUnbreakable
 		ModBlocks.register(modEventBus);
 		ModEntityTypes.register(modEventBus);
 
-		modEventBus.addListener(this::commonSetup);
-
 		MinecraftForge.EVENT_BUS.register(this);
 	}
 
-	private void commonSetup(final FMLCommonSetupEvent event) {
-		
-		Map<EntityType<?>, MergedSpawnPredicate<?>> mobSpawnMap = new HashMap<>();
-		// put Blue Slime Spawning into mobSpawnMap
-		mobSpawnMap.put(ModEntityTypes.BLUE_SLIME.get(), 
-				new MergedSpawnPredicate<>(Monster::checkMobSpawnRules, SpawnPlacements.Type.ON_GROUND, Heightmap.Types.WORLD_SURFACE));
-		
-		// create Spawn Placement Register Event
-		new SpawnPlacementRegisterEvent(mobSpawnMap);
-	}
-
-	
-	@Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-	public static class ClientModEvents {
-		@SuppressWarnings("removal")
-		@SubscribeEvent
-		public static void onClientSetup(FMLClientSetupEvent event) {
-			ItemBlockRenderTypes.setRenderLayer(ModBlocks.ONION_BLOCK.get(), RenderType.cutout());
-			EntityRenderers.register(ModEntityTypes.BLUE_SLIME.get(), BlueSlimeRenderer::new);
-		}
-	}
 }
--- forge-1.19.2-43.2.0-mdk/src/main/java/de/sajomon/bedrock_is_unbreakable/event/ModEvents.java
+++ forge-1.19.2-43.2.0-mdk/src/main/java/de/sajomon/bedrock_is_unbreakable/event/ModEvents.java
@@ -1,20 +1,72 @@
 package de.sajomon.bedrock_is_unbreakable.event;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import de.sajomon.bedrock_is_unbreakable.BedrockIsUnbreakable;
+import de.sajomon.bedrock_is_unbreakable.block.ModBlocks;
 import de.sajomon.bedrock_is_unbreakable.entity.ModEntityTypes;
+import de.sajomon.bedrock_is_unbreakable.entity.client.BlueSlimeRenderer;
 import de.sajomon.bedrock_is_unbreakable.entity.custom.BlueSlime;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.EntityRenderers;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
+import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent.MergedSpawnPredicate;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 @Mod.EventBusSubscriber(modid = BedrockIsUnbreakable.MOD_ID)
 public class ModEvents {
 	
+	private ModEvents() {
+		super();
+	}
+	
 	@Mod.EventBusSubscriber(modid = BedrockIsUnbreakable.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 	public static class ModEventBusEvents {
+		
+		private ModEventBusEvents() {
+			super();
+		}
+		
 		@SubscribeEvent
 		public static void entityAttributeEvent(EntityAttributeCreationEvent event) {
 			event.put(ModEntityTypes.BLUE_SLIME.get(), BlueSlime.setAttributes());
+		}
+		
+		@SubscribeEvent
+		public static void commonSetup(FMLCommonSetupEvent event) {
+			Map<EntityType<?>, MergedSpawnPredicate<?>> mobSpawnMap = new HashMap<>();
+			// put Blue Slime Spawning into mobSpawnMap
+			mobSpawnMap.put(ModEntityTypes.BLUE_SLIME.get(), 
+					new MergedSpawnPredicate<>(Monster::checkMobSpawnRules, SpawnPlacements.Type.ON_GROUND, Heightmap.Types.WORLD_SURFACE));
+			
+			// create Spawn Placement Register Event
+			new SpawnPlacementRegisterEvent(mobSpawnMap);
+		}
+	}
+	
+	@Mod.EventBusSubscriber(modid = BedrockIsUnbreakable.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+	public static class ClientModEvents {
+		
+		private ClientModEvents() {
+			super();
+		}
+		
+		@SuppressWarnings("removal")
+		@SubscribeEvent
+		public static void onClientSetup(FMLClientSetupEvent event) {
+			ItemBlockRenderTypes.setRenderLayer(ModBlocks.ONION_BLOCK.get(), RenderType.cutout());
+			EntityRenderers.register(ModEntityTypes.BLUE_SLIME.get(), BlueSlimeRenderer::new);
 		}
 	}
 
