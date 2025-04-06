f86dfcefbddf55b0f0682b385c317a4411df180e
snydto <sibekei@web.de>
2025-04-06 09:09:19 +0200

    change formatting (tabs to spaces)

--- forge-1.19.2-43.2.0-mdk/src/main/java/de/sajomon/bedrock_is_unbreakable/item/ModToolTiers.java
+++ forge-1.19.2-43.2.0-mdk/src/main/java/de/sajomon/bedrock_is_unbreakable/item/ModToolTiers.java
@@ -12,22 +12,21 @@ import net.minecraftforge.common.ForgeTier;
 import net.minecraftforge.common.TierSortingRegistry;
 
 public class ModToolTiers {
-	
-	public static final Tier OBSIDIAN;
-	public static final Tier OBSIDIAN_STEEL;
-	
-	static {
-		OBSIDIAN = TierSortingRegistry.registerTier(new ForgeTier(5, 1400,
-				10f, 4f, 15, ModTags.Blocks.NEEDS_OBSIDIAN_TOOL,
-				() -> Ingredient.of(ModItems.OBSIDIAN_INGOT.get())),
-				new ResourceLocation(BedrockIsUnbreakable.MOD_ID, "obsidian"),
-				List.of(Tiers.NETHERITE), List.of());
-		
-		OBSIDIAN_STEEL = TierSortingRegistry.registerTier(new ForgeTier(6, 2500,
-				12f, 4f, 20, ModTags.Blocks.NEEDS_OBSIDIAN_STEEL_TOOL,
-				() -> Ingredient.of(ModItems.OBSIDIAN_STEEL.get())),
-				new ResourceLocation(BedrockIsUnbreakable.MOD_ID, "obsidian_steel"),
-				List.of(ModToolTiers.OBSIDIAN), List.of());
-	}
-	
+
+    public static final Tier OBSIDIAN;
+    public static final Tier OBSIDIAN_STEEL;
+
+    static {
+        OBSIDIAN = TierSortingRegistry.registerTier(
+                new ForgeTier(5, 1400, 10f, 4f, 15, ModTags.Blocks.NEEDS_OBSIDIAN_TOOL,
+                        () -> Ingredient.of(ModItems.OBSIDIAN_INGOT.get())),
+                new ResourceLocation(BedrockIsUnbreakable.MOD_ID, "obsidian"), List.of(Tiers.NETHERITE), List.of());
+
+        OBSIDIAN_STEEL = TierSortingRegistry.registerTier(
+                new ForgeTier(6, 2500, 12f, 4f, 20, ModTags.Blocks.NEEDS_OBSIDIAN_STEEL_TOOL,
+                        () -> Ingredient.of(ModItems.OBSIDIAN_STEEL.get())),
+                new ResourceLocation(BedrockIsUnbreakable.MOD_ID, "obsidian_steel"), List.of(ModToolTiers.OBSIDIAN),
+                List.of());
+    }
+
 }
