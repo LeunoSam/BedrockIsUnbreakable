f20d681ad02f38a4ae476d65b73b93c544165ffc
snydto <sibekei@web.de>
2025-04-21 14:11:05 +0200

    make only bedrock destroyable by pickaxe action

--- forge-1.19.2-43.2.0-mdk/src/main/java/de/sajomon/bedrock_is_unbreakable/item/custom/ObsidianSteelPickaxeItem.java
+++ forge-1.19.2-43.2.0-mdk/src/main/java/de/sajomon/bedrock_is_unbreakable/item/custom/ObsidianSteelPickaxeItem.java
@@ -2,45 +2,45 @@ package de.sajomon.bedrock_is_unbreakable.item.custom;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 
 public class ObsidianSteelPickaxeItem extends PickaxeItem {
 
-	public ObsidianSteelPickaxeItem(Tier tier, int attackDamage, float attackSpeed, Properties props) {
-		super(tier, attackDamage, attackSpeed, props);
-	}
+    public ObsidianSteelPickaxeItem(Tier tier, int attackDamage, float attackSpeed, Properties props) {
+        super(tier, attackDamage, attackSpeed, props);
+    }
 
-	@Override
-	public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-		if (!level.isClientSide && hand == InteractionHand.MAIN_HAND) {
-			Vec3 direction = player.getViewVector(0).scale(0.01);
-			Vec3 eyes = player.getEyePosition(0);
-			Vec3 location = eyes.add(direction);
-			//selects first non-air-block within 4 blocks
-			while (level.getBlockState(new BlockPos(location)).toString().equals("Block{minecraft:air}")
-					&& absVec3(substractVec3(eyes, location)) <= 4) {
-				location = location.add(direction);
-			}
-			if (level.destroyBlock(new BlockPos(location), false)) {
-				player.getCooldowns().addCooldown(this, 5);
-				player.getMainHandItem().hurtAndBreak(10, player, p41625 -> p41625.broadcastBreakEvent(hand));
-			}
-		}
+    @Override
+    public InteractionResult useOn(UseOnContext useContext) {
+        Level level = useContext.getLevel();
+        Player player = useContext.getPlayer();
+        InteractionHand hand = useContext.getHand();
+        BlockPos pos = useContext.getClickedPos();
+        Block block = level.getBlockState(pos).getBlock();
 
-		return super.use(level, player, hand);
-	}
-	
-	private static double absVec3(Vec3 v) {
-		return Math.sqrt(v.x*v.x + v.y*v.y + v.z*v.z);
-	}
-	
-	private static Vec3 substractVec3(Vec3 v, Vec3 sub) {
-		return v.add(new Vec3(-sub.x, -sub.y, -sub.z));
-	}
+        if (isActionAllowed(hand, level) && block.equals(Blocks.BEDROCK)) {
+            destroyBlock(player, hand, level, pos);
+        }
+
+        return super.useOn(useContext);
+    }
+
+    private boolean isActionAllowed(InteractionHand hand, Level level) {
+        return !level.isClientSide && hand == InteractionHand.MAIN_HAND;
+    }
+
+    private void destroyBlock(Player player, InteractionHand hand, Level level, BlockPos pos) {
+        if (level.destroyBlock(pos, false)) {
+            player.getCooldowns().addCooldown(this, 5);
+            player.getMainHandItem().hurtAndBreak(10, player, p41625 -> p41625.broadcastBreakEvent(hand));
+        }
+    }
+
 }
--- forge-1.20.1-47.4.0-mdk/src/main/java/de/sajomon/bedrock_is_unbreakable/item/custom/ObsidianSteelPickaxeItem.java
+++ forge-1.20.1-47.4.0-mdk/src/main/java/de/sajomon/bedrock_is_unbreakable/item/custom/ObsidianSteelPickaxeItem.java
@@ -2,13 +2,14 @@ package de.sajomon.bedrock_is_unbreakable.item.custom;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 
 public class ObsidianSteelPickaxeItem extends PickaxeItem {
 
@@ -17,31 +18,29 @@ public class ObsidianSteelPickaxeItem extends PickaxeItem {
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-        if (!level.isClientSide && hand == InteractionHand.MAIN_HAND) {
-            Vec3 direction = player.getViewVector(0).scale(0.01);
-            Vec3 eyes = player.getEyePosition(0);
-            Vec3 location = eyes.add(direction);
+    public InteractionResult useOn(UseOnContext useContext) {
+        Level level = useContext.getLevel();
+        Player player = useContext.getPlayer();
+        InteractionHand hand = useContext.getHand();
+        BlockPos pos = useContext.getClickedPos();
+        Block block = level.getBlockState(pos).getBlock();
 
-            // selects first non-air-block within 4 blocks
-            while (level.getBlockState(BlockPos.containing(location.x, location.y, location.z)).toString()
-                    .equals("Block{minecraft:air}") && absVec3(substractVec3(eyes, location)) <= 4) {
-                location = location.add(direction);
-            }
-            if (level.destroyBlock(BlockPos.containing(location.x, location.y, location.z), false)) {
-                player.getCooldowns().addCooldown(this, 5);
-                player.getMainHandItem().hurtAndBreak(10, player, p41625 -> p41625.broadcastBreakEvent(hand));
-            }
+        if (isActionAllowed(hand, level) && block.equals(Blocks.BEDROCK)) {
+            destroyBlock(player, hand, level, pos);
         }
 
-        return super.use(level, player, hand);
+        return super.useOn(useContext);
     }
 
-    private static double absVec3(Vec3 v) {
-        return Math.sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+    private boolean isActionAllowed(InteractionHand hand, Level level) {
+        return !level.isClientSide && hand == InteractionHand.MAIN_HAND;
     }
 
-    private static Vec3 substractVec3(Vec3 v, Vec3 sub) {
-        return v.add(new Vec3(-sub.x, -sub.y, -sub.z));
+    private void destroyBlock(Player player, InteractionHand hand, Level level, BlockPos pos) {
+        if (level.destroyBlock(pos, false)) {
+            player.getCooldowns().addCooldown(this, 5);
+            player.getMainHandItem().hurtAndBreak(10, player, p41625 -> p41625.broadcastBreakEvent(hand));
+        }
     }
+
 }
