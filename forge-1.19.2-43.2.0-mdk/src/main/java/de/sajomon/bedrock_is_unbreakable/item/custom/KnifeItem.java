cb7ab2477f06dd3b7eccf8848f70e4b8264ca74e
snydto <sibekei@web.de>
2025-04-21 21:36:32 +0200

    add WeepingEffect

--- forge-1.19.2-43.2.0-mdk/src/main/java/de/sajomon/bedrock_is_unbreakable/item/custom/KnifeItem.java
+++ forge-1.19.2-43.2.0-mdk/src/main/java/de/sajomon/bedrock_is_unbreakable/item/custom/KnifeItem.java
@@ -21,59 +21,61 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 
 public class KnifeItem extends SwordItem {
-	
-	private int pos;
-	private Random rand;
 
-	/**
-	 * Creates SwordItem with the given params.
-	 * Possibility has to be between 0 and 100.
-	 * @param tier
-	 * @param attackDamage
-	 * @param attackSpeed
-	 * @param properties
-	 * @param possibility If possibility is for example 20 there is a 20% chance that the player gets a tear while
-	 * slicing onions. If possibility <= 0, there is a 0% chance, else if possibility >= 100, there is a 100% chance.
-	 */
-	public KnifeItem(Tier tier, int attackDamage, float attackSpeed, Properties properties, int possibility) {
-		super(tier, attackDamage, attackSpeed, properties);
-		this.pos = possibility;
-		rand = new Random();
-	}
-	
-	
-	@Override
-	public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-		if(!level.isClientSide && hand == InteractionHand.MAIN_HAND) {
-			Inventory i = player.getInventory();
-			int slot = i.findSlotMatchingItem(new ItemStack(ModItems.ONION.get()));
-			if(slot != -1) {
-				i.getItem(slot).shrink(1);
-				player.drop(new ItemStack(ModItems.SLICED_ONION.get(), 4), true);
-				if(rand.nextInt(100)+1 <= pos) {
-					i.add(new ItemStack(ModItems.PLAYER_TEARS.get()));
-					player.hurt(DamageSource.sting(player), 1f);
-					player.giveExperiencePoints(2);
-				}
-				player.getMainHandItem().hurtAndBreak(10, player, p41625 ->
-					p41625.broadcastBreakEvent(hand)
-				);
-				player.getCooldowns().addCooldown(this, 5);
-			}
-		}
-		return super.use(level, player, hand);
-	}
+    private int pos;
+    private Random rand;
 
-	@Override
-	public void appendHoverText(ItemStack itemStack, Level level, List<Component> components, TooltipFlag tooltipFlag) {
-		if(!Screen.hasShiftDown()) {
-			components.add(Component.translatable(new ResourceLocation(BedrockIsUnbreakable.MOD_ID,"press_shift")
-					.toString()).withStyle(ChatFormatting.GRAY));
-		} else {
-			components.add(Component.translatable(new ResourceLocation(BedrockIsUnbreakable.MOD_ID,"info_knife_item")
-					.toString()).withStyle(ChatFormatting.YELLOW));
-		}
-		super.appendHoverText(itemStack, level, components, tooltipFlag);
-	}
-	
+    /**
+     * Creates SwordItem with the given params. Possibility has to be between 0 and
+     * 100.
+     * 
+     * @param tier
+     * @param attackDamage
+     * @param attackSpeed
+     * @param properties
+     * @param possibility  If possibility is for example 20 there is a 20% chance
+     *                     that the player gets a tear while slicing onions. If
+     *                     possibility <= 0, there is a 0% chance, else if
+     *                     possibility >= 100, there is a 100% chance.
+     */
+    public KnifeItem(Tier tier, int attackDamage, float attackSpeed, Properties properties, int possibility) {
+        super(tier, attackDamage, attackSpeed, properties);
+        this.pos = possibility;
+        rand = new Random();
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        if (!level.isClientSide && hand == InteractionHand.MAIN_HAND) {
+            Inventory i = player.getInventory();
+            int slot = i.findSlotMatchingItem(new ItemStack(ModItems.ONION.get()));
+            if (slot != -1) {
+                i.getItem(slot).shrink(1);
+                player.drop(new ItemStack(ModItems.SLICED_ONION.get(), 4), true);
+                if (rand.nextInt(100) + 1 <= pos) {
+                    i.add(new ItemStack(ModItems.PLAYER_TEARS.get()));
+                    player.hurt(DamageSource.sting(player), 1f);
+                    player.giveExperiencePoints(2);
+                }
+                player.getMainHandItem().hurtAndBreak(10, player, p41625 -> p41625.broadcastBreakEvent(hand));
+                player.getCooldowns().addCooldown(this, 5);
+            }
+        }
+        return super.use(level, player, hand);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, Level level, List<Component> components, TooltipFlag tooltipFlag) {
+        if (!Screen.hasShiftDown()) {
+            components.add(
+                    Component.translatable(new ResourceLocation(BedrockIsUnbreakable.MOD_ID, "press_shift").toString())
+                            .withStyle(ChatFormatting.GRAY));
+        } else {
+            components.add(Component
+                    .translatable(new ResourceLocation(BedrockIsUnbreakable.MOD_ID, "info_knife_item").toString())
+                    .withStyle(ChatFormatting.YELLOW));
+        }
+        super.appendHoverText(itemStack, level, components, tooltipFlag);
+    }
+
 }
---
+++ forge-1.19.2-43.2.0-mdk/src/main/java/de/sajomon/bedrock_is_unbreakable/world/effect/custom/WeepingEffect.java
@@ -0,0 +1,12 @@
+package de.sajomon.bedrock_is_unbreakable.world.effect.custom;
+
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+
+public class WeepingEffect extends MobEffect {
+
+    public WeepingEffect(MobEffectCategory category, int color) {
+        super(category, color);
+    }
+
+}
