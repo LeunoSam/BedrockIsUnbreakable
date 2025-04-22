bf1ba50158b065cb71d3c151b028970d26126785
snydto <sibekei@web.de>
2025-04-22 17:17:48 +0200

    init neoforged 1.21.1

---
+++ neoforged-21.1.161-mdk/src/main/java/de/sajomon/bedrock_is_unbreakable/BedrockIsUnbreakable.java
@@ -0,0 +1,33 @@
+package de.sajomon.bedrock_is_unbreakable;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(BedrockIsUnbreakable.MOD_ID)
+public class BedrockIsUnbreakable {
+
+    public static final String MOD_ID = "bedrock_is_unbreakable";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public BedrockIsUnbreakable(IEventBus modEventBus, ModContainer modContainer) {
+
+//        ModItems.register(modEventBus);
+//        ModBlocks.register(modEventBus);
+//        ModCreativeModeTabs.register(modEventBus);
+//
+//        ModMobEffects.register(modEventBus);
+//        ModPotions.register(modEventBus);
+//
+//        ModEntityTypes.register(modEventBus);
+//        ModParticles.register(modEventBus);
+
+    }
+
+}
